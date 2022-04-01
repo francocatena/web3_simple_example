886479cd1f13abc655a64c05d3b919c9c5e41f0c
Franco Catena <francocatena@gmail.com>
2022-04-01 09:26:37 -0300

    Single page web3 connection sample

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,266 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+const abi = [
+  {
+    'inputs': [
+      {
+        'internalType': 'string',
+        'name': '_greeting',
+        'type': 'string'
+      }
+    ],
+    'stateMutability': 'nonpayable',
+    'type': 'constructor'
+  },
+  {
+    'inputs': [],
+    'name': 'greet',
+    'outputs': [
+      {
+        'internalType': 'string',
+        'name': '',
+        'type': 'string'
+      }
+    ],
+    'stateMutability': 'view',
+    'type': 'function'
+  },
+  {
+    'inputs': [
+      {
+        'internalType': 'string',
+        'name': '_greeting',
+        'type': 'string'
+      }
+    ],
+    'name': 'setGreeting',
+    'outputs': [],
+    'stateMutability': 'nonpayable',
+    'type': 'function'
+  }
+]
+
+const styles = {
+  accountDisconnected: [
+    'bg-blue-500',
+    'hover:bg-blue-700',
+    'text-white',
+    'font-bold',
+    'py-2',
+    'px-4',
+    'rounded',
+    'focus:outline-none',
+    'focus:shadow-outline'
+  ].join(' '),
+  accountConnected: [
+    'bg-transparent',
+    'hover:bg-blue-500',
+    'text-blue-700',
+    'font-semibold',
+    'hover:text-white',
+    'py-2',
+    'px-4',
+    'border',
+    'border-blue-500',
+    'hover:border-transparent',
+    'rounded'
+  ].join(' '),
+  input: [
+    'appearance-none',
+    'bg-transparent',
+    'border-none',
+    'w-full',
+    'text-gray-700',
+    'mr-3',
+    'py-1',
+    'px-2',
+    'leading-tight',
+    'focus:outline-none'
+  ].join(' '),
+  submit: [
+    'bg-blue-500',
+    'hover:bg-blue-700',
+    'text-white',
+    'font-bold',
+    'py-2',
+    'px-4',
+    'rounded',
+    'disabled:opacity-50',
+    'focus:outline-none',
+    'focus:shadow-outline'
+  ].join(' '),
+  refresh: [
+    'bg-blue-500',
+    'hover:bg-blue-700',
+    'text-white',
+    'font-bold',
+    'py-2',
+    'px-4',
+    'rounded',
+    'focus:outline-none',
+    'focus:shadow-outline'
+  ].join(' '),
+  greeting: [
+    'bg-blue-100',
+    'border-blue-500',
+    'text-blue-700',
+    'px-4',
+    'py-3',
+    'mb-6'
+  ].join(' '),
+  noGreeting: [
+    'bg-red-100',
+    'border-red-500',
+    'text-red-700',
+    'px-4',
+    'py-3',
+    'mb-6'
+  ].join(' ')
+}
+
+const Account = props => {
+  const connect = () => {
+    ethereum.request({
+      method: 'eth_requestAccounts'
+    }).then(accounts => {
+      if (accounts.length === 0) {
+        console.log('Please connect to MetaMask.')
+      } else if (accounts[0] !== props.currentAccount) {
+        props.setCurrentAccount(accounts[0])
+      }
+    }).catch(error => {
+      if (error.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to MetaMask.')
+      } else {
+        console.error(error)
+      }
+    })
+  }
+
+  if (props.currentAccount) {
+    return (
+      <div className={styles.accountConnected}>
+        {props.currentAccount}
+      </div>
+    )
+  } else {
+    return (
+      <button type="button" className={styles.accountDisconnected} onClick={connect}>
+        Conectar
+      </button>
+    )
+  }
+}
+
 const Home: NextPage = () => {
+  const [currentAccount, setCurrentAccount] = useState()
+  const [currentGreeting, setCurrentGreeting] = useState()
+
+  let provider
+
+  const getProvider = () => {
+    if (! provider) {
+      provider = new ethers.providers.Web3Provider(window.ethereum)
+    }
+
+    return provider
+  }
+
+  let contract
+
+  const getContract = () => {
+    if (! contract) {
+      const provider = getProvider()
+
+      contract = new ethers.Contract(address, abi, provider)
+    }
+
+    return contract
+  }
+
+  let signer
+
+  const getSigner = () => {
+    if (! signer) {
+      const provider = getProvider()
+
+      signer = provider.getSigner()
+    }
+
+    return signer
+  }
+
+  const refreshGreeting = async () => {
+    const contract = getContract()
+
+    setCurrentGreeting(await contract.greet())
+  }
+
+  const setGreeting = event => {
+    event.preventDefault()
+
+    if (currentAccount) {
+      const greeting = event.target.greeting.value
+      const contract = getContract()
+      const signer   = getSigner()
+
+      contract.connect(signer).setGreeting(greeting).then(() => {
+        event.target.greeting.value = ''
+      })
+    }
+  }
+
+  useEffect(() => {
+    refreshGreeting()
+  })
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Bienvenidos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold mb-6">
+          ¡Bienvenidos!
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="mb-6">
+          <Account currentAccount={currentAccount} setCurrentAccount={setCurrentAccount} />
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+        <form className="w-full max-w-sm mb-6" onSubmit={setGreeting}>
+          <div className="flex items-center border-b border-teal-500 py-2">
+            <input name="greeting"
+                   type="text"
+                   className={styles.input}
+                   placeholder="Escriba un nuevo mensaje"
+                   autoComplete="off"
+                   disabled={! currentAccount} />
+              <button type="submit" className={styles.submit} disabled={! currentAccount}>
+                Modificar
+              </button>
+            </div>
+        </form>
+
+        <div className={currentGreeting ? styles.greeting : styles.noGreeting}>
+          {currentGreeting || 'No hay ningún mensaje establecido'}
+        </div>
+
+        <p>
+          <button type="button" className={styles.refresh} onClick={refreshGreeting}>
+            Refrescar saludo
+          </button>
+        </p>
+      </main>
     </div>
   )
 }
